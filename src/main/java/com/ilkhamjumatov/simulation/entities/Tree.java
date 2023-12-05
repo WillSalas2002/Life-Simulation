19b168bfc2dd1ad07b67f29823f72a339dc6d11f
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-05 09:45:14 +0500

    Created Tree.java class

---
+++ src/main/java/com/ilkhamjumatov/simulation/entities/Tree.java
@@ -0,0 +1,13 @@
+package com.ilkhamjumatov.simulation.entities;
+
+public class Tree extends Entity {
+
+    public Tree() {
+        name = "Tree";
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
