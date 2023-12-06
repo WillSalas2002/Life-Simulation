f70b7e2d079699961a30b42163682360234c92c7
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-06 16:10:02 +0500

    Small updates in Creature.java class

--- src/main/java/com/ilkhamjumatov/simulation/entities/Creature.java
+++ src/main/java/com/ilkhamjumatov/simulation/entities/Creature.java
@@ -1,9 +1,12 @@
 package com.ilkhamjumatov.simulation.entities;
 
+import com.ilkhamjumatov.simulation.Coordinate;
+
 public abstract class Creature extends Entity {
 
+    protected PathFinder pathFinder;
     protected int hp;
 
-    public abstract void makeMove();
+    public abstract void makeMove(Coordinate coordinate);
 
 }
