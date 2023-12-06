a0bbf54aabf04a327d7c964893c500701ac6d18b
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-06 20:35:15 +0500

    Action.java: implemented turnAction() method

--- src/main/java/com/ilkhamjumatov/simulation/Action.java
+++ src/main/java/com/ilkhamjumatov/simulation/Action.java
@@ -2,15 +2,29 @@ package com.ilkhamjumatov.simulation;
 
 import com.ilkhamjumatov.simulation.entities.*;
 
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 public class Action {
 
     private final Entity[] entities = {new Rock(), new Tree(), new Grass(), new Herbivore(), new Predator()};
+    private final Random random = new Random();
 
     public void initAction(GameMap gameMap) {
         populateMap(gameMap);
+    }
+
+    public void turnAction() {
+
+        Set<Coordinate> coordinatesOfAllEntities = new HashSet<>(GameMap.entityCoordinateMap.keySet());
+
+        for (Coordinate coordinate : coordinatesOfAllEntities) {
+
+            Entity entity = GameMap.entityCoordinateMap.get(coordinate);
+
+            if (entity instanceof Creature creature) {
+                creature.makeMove(coordinate);
+            }
+        }
     }
 
     private void populateMap(GameMap gameMap) {
@@ -20,18 +34,16 @@ public class Action {
 
         for (int i = 0; i < (rows * columns) / 10; i++) {
 
-            Random random = new Random();
-
             int xRandom = random.nextInt(rows);
             int yRandom = random.nextInt(columns);
 
             Entity randomEntity = entities[random.nextInt(entities.length)];
+            Coordinate randomCoordinate = new Coordinate(xRandom, yRandom);
 
-            if (!GameMap.entityCoordinateMap.containsKey(new Coordinate(xRandom, yRandom))) {
-
-                GameMap.entityCoordinateMap.put(new Coordinate(xRandom, yRandom), randomEntity);
+            if (!GameMap.entityCoordinateMap.containsKey(randomCoordinate)) {
+                randomEntity.setCoordinate(randomCoordinate);
+                GameMap.entityCoordinateMap.put(randomCoordinate, randomEntity);
             }
-
         }
     }
 }
