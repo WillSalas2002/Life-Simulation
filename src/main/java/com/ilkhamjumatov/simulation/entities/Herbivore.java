b29be684f236cd6bb1815d0eddc851472be4f5f2
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-06 16:12:10 +0500

    Herbivore.class: implemented makeMove() method

--- src/main/java/com/ilkhamjumatov/simulation/entities/Herbivore.java
+++ src/main/java/com/ilkhamjumatov/simulation/entities/Herbivore.java
@@ -1,6 +1,13 @@
 package com.ilkhamjumatov.simulation.entities;
 
+import com.ilkhamjumatov.simulation.Coordinate;
+import com.ilkhamjumatov.simulation.GameMap;
+
+import java.util.List;
+import java.util.Map;
+
 public class Herbivore extends Creature {
+
 
     public Herbivore() {
         hp = 90;
@@ -13,7 +20,20 @@ public class Herbivore extends Creature {
     }
 
     @Override
-    public void makeMove() {
+    public void makeMove(Coordinate coordinate) {
 
+        List<Coordinate> excludingGrass = GameMap.excludingGrass();
+        List<Coordinate> coordinates = PathFinder.breadthFirstSearch(coordinate, GameMap.getGrassCoordinate(), excludingGrass);
+
+        if (coordinates == null) {
+            hp -= 30;
+        } else if (coordinates.size() == 1) {
+            GameMap.entityCoordinateMap.remove(coordinates.get(0));
+        } else {
+            Coordinate newCoordinate = coordinates.get(0);
+            GameMap.entityCoordinateMap.remove(coordinate);
+            this.setCoordinate(newCoordinate);
+            GameMap.entityCoordinateMap.put(newCoordinate, this);
+        }
     }
 }
