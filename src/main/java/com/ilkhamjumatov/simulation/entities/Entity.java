f15f137facc98de4775deef37cdee8f33482caa7
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-06 16:10:25 +0500

    Small updates in Entity class

--- src/main/java/com/ilkhamjumatov/simulation/entities/Entity.java
+++ src/main/java/com/ilkhamjumatov/simulation/entities/Entity.java
@@ -1,8 +1,19 @@
 package com.ilkhamjumatov.simulation.entities;
 
+import com.ilkhamjumatov.simulation.Coordinate;
+
 public abstract class Entity {
 
     protected String name;
+    protected Coordinate coordinate;
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
 
     public Entity() {
 
