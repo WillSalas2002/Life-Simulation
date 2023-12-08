dc893e9dc1693d9714e25e82cd7da1fbdfad0256
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-08 23:04:01 +0500

    Update: changed the logic for generating random coordinate and random entity

--- src/main/java/com/ilkhamjumatov/simulation/InitAction.java
+++ src/main/java/com/ilkhamjumatov/simulation/InitAction.java
@@ -17,61 +17,30 @@ public class InitAction extends Action {
 
     private void populateMap(GameMap gameMap) {
 
-        int rows = gameMap.getRows();
-        int columns = gameMap.getColumns();
+        Predator pr = new Predator();
+        pr.setCoordinate(new Coordinate(0,0));
+        gameMap.getEntityCoordinateMap().put(new Coordinate(0,0), pr);
 
-//        Coordinate c1 = new Coordinate(0,0);
-//        Entity e1 = new Predator();
-//        e1.setCoordinate(c1);
-//
-//        Coordinate c2 = new Coordinate(0,9);
-//        Entity e2 = new Predator();
-//        e2.setCoordinate(c2);
-//
-//        Coordinate c3 = new Coordinate(9,0);
-//        Entity e3 = new Herbivore();
-//        e3.setCoordinate(c3);
-//
-//        Coordinate c4 = new Coordinate(9,9);
-//        Entity e4 = new Herbivore();
-//        e4.setCoordinate(c4);
-//
-//        Coordinate c5 = new Coordinate(5,5);
-//        Entity e5 = new Herbivore();
-//        e5.setCoordinate(c5);
-//
-//        Coordinate c6 = new Coordinate(2,2);
-//        Entity e6 = new Grass();
-//        e6.setCoordinate(c6);
-//
-//        gameMap.getEntityCoordinateMap().put(c1,e1);
-//        gameMap.getEntityCoordinateMap().put(c2,e2);
-//        gameMap.getEntityCoordinateMap().put(c3,e3);
-//        gameMap.getEntityCoordinateMap().put(c4,e4);
-//        gameMap.getEntityCoordinateMap().put(c5,e5);
-//        gameMap.getEntityCoordinateMap().put(c6,e6);
+        for (int i = 0; i < GameMap.COLUMNS * GameMap.COLUMNS * 0.4; i++) {
 
-        for (int i = 0; i < (rows * columns) / 10; i++) {
-
-            int xRandom = random.nextInt(rows);
-            int yRandom = random.nextInt(columns);
-
-            Coordinate randomCoordinate = new Coordinate(xRandom, yRandom);
-
-            if (!gameMap.getEntityCoordinateMap().containsKey(randomCoordinate)) {
-                Entity randomEntity = entities[random.nextInt(entities.length)];
-                randomEntity.setCoordinate(randomCoordinate);
-                gameMap.addEntity(randomEntity);
-                System.out.println(randomEntity.getCoordinate().equals(randomCoordinate));
-            }
+            Entity entity = getRandomEntity(entities[random.nextInt(entities.length - 1)].getClass());
+            gameMap.addEntity(entity);
         }
+    }
 
+    private Entity getRandomEntity(Class<? extends Entity> entityClass) {
 
-        for (Map.Entry<Coordinate, Entity> entry : gameMap.getEntityCoordinateMap().entrySet()) {
-            Entity entity = entry.getValue();
-            Coordinate coordinate = entry.getKey();
+        int randomCol = random.nextInt(GameMap.COLUMNS);
+        int randomRow = random.nextInt(GameMap.ROWS);
 
-            System.out.println(entity + " : " + entity.getCoordinate() + " : " + coordinate);
+        try {
+            Entity entity = entityClass.getConstructor().newInstance();
+            entity.setCoordinate(new Coordinate(randomCol, randomRow));
+
+            return entity;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
